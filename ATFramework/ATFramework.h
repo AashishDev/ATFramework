b27d2ffd4b5b3753a32c046f42a95061618a8b67
Aashish Tyagi <aashish.tyagi@crmnext.in>
2017-05-25 16:03:28 +0530

    Intial commit

---
+++ ATFramework/ATFramework.h
@@ -0,0 +1,19 @@
+//
+//  ATFramework.h
+//  ATFramework
+//
+//  Created by MOBILE MAC1 on 5/25/17.
+//  Copyright © 2017 MOBILE MAC1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATFramework.
+FOUNDATION_EXPORT double ATFrameworkVersionNumber;
+
+//! Project version string for ATFramework.
+FOUNDATION_EXPORT const unsigned char ATFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATFramework/PublicHeader.h>
+
+
---
+++ ATFramework/ATLog.swift
@@ -0,0 +1,17 @@
+//
+//  ATLog.swift
+//  ATFramework
+//
+//  Created by MOBILE MAC1 on 5/25/17.
+//  Copyright © 2017 MOBILE MAC1. All rights reserved.
+//
+
+import UIKit
+
+ public class ATLog: NSObject {
+
+    public func showMsg(){
+        print("Nice to use ATFramework");
+    }
+    
+}
